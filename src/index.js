aa2b14b3a4fe3cdc9e4465db3047585eda7a5a17
Fwep <tarancacacho@gmail.com>
2019-01-14 10:31:43 -0800

    Write the main Javascript file
    This file includes three.js code to set up a scene, complete
    with a starfield. The starfield is simply just a sphere in which
    the camera is inside of.
    
    The planets will be rendered in the middle of the scene, and are also
    three.js spheres with topographical and/or atmosphere maps pulled from
    PlanetPixelEmporium hi-res photos layered on top.
    
    Camera controls are also included, and the animation loop includes code
    to account for camera movement whenever the user moves his/her mouse.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,68 @@
 import * as THREE from "three";
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-var renderer = new THREE.WebGLRenderer();
+/* Set the scene up */
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.shadowMapEnabled = true // Turn on shadows in the renderer
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100);
+camera.position.z = 1;
 
-camera.position.z = 5;
+/* The Starfield: it's simple a three.js sphere */
+var geometry = new THREE.SphereGeometry(40, 32, 32);
+var material = new THREE.MeshBasicMaterial();
+material.map = new THREE.TextureLoader().load("../starfield.png");
+material.side = THREE.BackSide;
+var mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-var animate = function () {
-  requestAnimationFrame(animate);
+/* MeshPhongMaterial won't work if you don't have a light source */
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 1, 1).normalize();
+scene.add(light);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+/* Conditionally render the planet */
 
+var geometry = new THREE.SphereGeometry(0.5, 32, 32)
+var material = new THREE.MeshPhongMaterial({
+  map: new THREE.TextureLoader().load('/src/planets/neptune/neptunemap.jpg'),
+  bumpMap: new THREE.TextureLoader().load('/src/planets/neptune/neptunemap.jpg'),
+  bumpScale: 0.005,
+})
+var mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh);
+
+// Camera controls
+var cameraPositions = [];
+var mouse = { x: 0, y: 0 }
+document.addEventListener('mousemove', function (event) {
+  mouse.x = (event.clientX / window.innerWidth) - 0.5
+  mouse.y = (event.clientY / window.innerHeight) - 0.5
+}, false) // The boolean means that the listener should NOT be invoked at most once
+
+cameraPositions.push(function (delta, now) {
+  camera.position.x += (mouse.x * 5 - camera.position.x) * (delta * 3)
+  camera.position.y += (mouse.y * 5 - camera.position.y) * (delta * 3)
+  camera.lookAt(scene.position)
+})
+
+// Renders any camera movements
+cameraPositions.push(function () {
   renderer.render(scene, camera);
-};
+})
 
-animate();
+/* Animation loop */
+var lastTimeMsec = null
+requestAnimationFrame(function animate(nowMsec) {
+  requestAnimationFrame(animate);
+  lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60
+  var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
+  lastTimeMsec = nowMsec
+  cameraPositions.forEach(function (updateFn) {
+    updateFn(deltaMsec / 1000, nowMsec / 1000)
+  })
+});
