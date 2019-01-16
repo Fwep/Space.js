5d1f548d6f3d96a89dafbc754a264100c4011db0
Fwep <tarancacacho@gmail.com>
2019-01-16 09:40:04 -0800

    Modify image file paths

--- src/planets/jupiter/jupiter.js
+++ src/planets/jupiter/jupiter.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 var geometry = new THREE.SphereGeometry(0.5, 32, 32);
 var material = new THREE.MeshPhongMaterial({
-  map: new THREE.TextureLoader().load("/src/planets/jupiter/jupitermap.jpg"),
+  map: new THREE.TextureLoader().load("./src/planets/jupiter/jupitermap.jpg"),
   bumpMap: new THREE.TextureLoader().load(
     "/src/planets/jupiter/jupitermap.jpg"
   ),
--- src/planets/venus/venus.js
+++ src/planets/venus/venus.js
@@ -2,8 +2,8 @@ import * as THREE from "three";
 
 var geometry = new THREE.SphereGeometry(0.5, 32, 32);
 var material = new THREE.MeshPhongMaterial({
-  map: new THREE.TextureLoader().load("/src/planets/venus/venusmap.jpg"),
-  bumpMap: new THREE.TextureLoader().load("/src/planets/venus/venusbump.jpg"),
+  map: new THREE.TextureLoader().load("./src/planets/venus/venusmap.jpg"),
+  bumpMap: new THREE.TextureLoader().load("./src/planets/venus/venusbump.jpg"),
   bumpScale: 0.05
 });
 var venusMesh = new THREE.Mesh(geometry, material);
